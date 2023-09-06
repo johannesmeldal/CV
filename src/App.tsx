eec3a018579689d307475b1116bfaef2ccce014d
Johannes <johanlm@stud.ntnu.no>
2023-09-06 15:38:38 +0200

    added darkmode yey

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, Button } from '@mui/material';
+import { MdOutlineDarkMode, MdLightMode } from 'react-icons/md';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {
+    darkmode: boolean;
+    setDarkmode: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default App;
+const App = ({darkmode, setDarkmode} : Props) => {
+
+    const handleDarkmode = (props: Props) => {
+        darkmode ? setDarkmode(false) : setDarkmode(true);
+        // if (darkmode === 'true') {
+        //     console.log('darkmode is true');
+        //     setDarkmode('false');
+        // } else {
+        //     console.log('darkmode is false');
+        //     setDarkmode('true');
+        // }
+    }
+    
+  return (
+    
+      <Container maxWidth='xl' >
+      <Button fullWidth variant="contained" onClick={
+        () => handleDarkmode({darkmode, setDarkmode}) //handleDarkmode må ha med props for å kunne endre på darkmode
+        }>
+        {darkmode ? <MdOutlineDarkMode/> : <MdLightMode/>}
+      </Button>
+
+    </Container>
+  )
+}
+
+export default App
